ea4b629850ce29fe8a3f451725d12f40a30fea0d
Sanjit Dutta <sdlynx@gmail.com>
2016-04-16 16:57:05 -0500

    basic server.js set

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Get the packages we need
 var express = require('express');
 var mongoose = require('mongoose');
-var Llama = require('./models/llama');
+// var User = require('./models/user');
+// var Habit = require('./models/habit');
 var bodyParser = require('body-parser');
 var router = express.Router();
 
-//replace this with your Mongolab URL
-mongoose.connect('mongodb://localhost/mp4');
+// mongoose.connect('mongodb://insecureUserName:unsecuredPassword@ds017070.mlab.com:17070/cs-498rk-mp4-back');
 
 // Create our Express application
 var app = express();
@@ -14,38 +14,59 @@ var app = express();
 // Use environment defined port or 4000
 var port = process.env.PORT || 4000;
 
+/*
+
 //Allow CORS so that backend and frontend could pe put on different servers
 var allowCrossDomain = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
-  next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+	next();
 };
 app.use(allowCrossDomain);
 
+*/
+
 // Use the body-parser package in our application
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
+
+// this does something?
+app.use(bodyParser.json());
+
+// preflight stuffs from https://gist.github.com/cuppster/2344435
+// app.use(express.methodOverride());
+
+// ## CORS middleware
+// 
+// see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+var allowCrossDomain = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+// intercept OPTIONS method
+if ('OPTIONS' == req.method) {
+	res.sendStatus(200);
+}
+else {
+	next();
+}
+};
+app.use(allowCrossDomain);
 
 // All our routes will start with /api
 app.use('/api', router);
 
-//Default route here
 var homeRoute = router.route('/');
-
+var usersRoute = router.route('/users');
+var userRoute = router.route('/users/:id');
+var habitsRoute = router.route('/habits');
+var habitRoute = router.route('/habits/:id');
+ 
 homeRoute.get(function(req, res) {
-  res.json({ message: 'Hello World!' });
+	res.status(404).json({
+		message : 'Nothing here. Go to ./users or ./habits to play with the API.',
+		data : []
+	});
 });
-
-//Llama route
-var llamaRoute = router.route('/llamas');
-
-llamaRoute.get(function(req, res) {
-  res.json([{ "name": "alice", "height": 12 }, { "name": "jane", "height": 13 }]);
-});
-
-//Add more routes here
-
-// Start the server
-app.listen(port);
-console.log('Server running on port ' + port);
